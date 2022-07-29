d349c1d623d4c81d5b55f6c76c7393bcd0730150
João Marcelo Dantas <jmgrd98@gmail.com>
2022-07-29 18:10:16 -0300

    axios commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send('Olá joao!');
+    axios.get('https://api.github.com/users/jmgrd98')
 } )
 
 app.listen(3000, () => console.log('Olá joao'));
