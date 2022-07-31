70b4551eb34b948619c00414d9500e016dd2edcb
João Marcelo Dantas <jmgrd98@gmail.com>
2022-07-31 19:54:43 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const axios = require('axios');
 const app = express();
 
-app.route("/").get((req, res) => {
-    axios.get('https://api.github.com/users/jmgrd98')
-    .then(result => res.send(result.data))
-    .catch(error => console.error(error))
-} )
+//app.route("/").get((req, res) => {
+    //axios.get('https://api.github.com/users/jmgrd98')
+    //.then(result => res.send(result.data))
+   // .catch(error => console.error(error))
+//} )
+
+app.use(express.json());
+
+app.route("/").post((req, res) => console.log(req.body));
+
+
 
 app.listen(3000, () => console.log('Olá joao'));
